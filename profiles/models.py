aa982c9e87deb6aeb8c3a5ac4206e7b5ecebdd1b
ReinerYaeger <chevking101@gmail.com>
2023-12-28 07:06:37 -0500

    update

--- profiles/models.py
+++ profiles/models.py
@@ -1,13 +1,3 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-class Profile(AbstractUser):
-    name = models.CharField("Name", max_length=240)
-    email = models.EmailField()
-    password = models.CharField(max_length=50)
-
-    registrationDate = models.DateField("Registration Date", auto_now_add=True)
-
-    def __str__(self):
-        return self.name
