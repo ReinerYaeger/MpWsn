26061137c1f473dbee5f2e7d70f062b55baf2fad
ReinerYaeger <chevking101@gmail.com>
2024-01-15 18:14:28 -0500

    reverted to old arduino_interface.py

--- iot/pi_files/arduino_interface.py
+++ iot/pi_files/arduino_interface.py
@@ -1,90 +1,30 @@
 # Code for the Raspberry pi
 import asyncio
+import base64
 import pickle
-import threading
 from time import sleep
 import serial
 import traceback
 from datetime import datetime
 import logging
 import socket
-import random
+#from cryptography.fernet import Fernet
+import os
+from dotenv import load_dotenv
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', filename='mp_wn.log', encoding='utf-8',
                     level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-file_path = "sensor_location.txt"
-
-
-def get_random_data_from_file():
-    try:
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-            random_line = random.choice(lines)
-
-            sensor_group_name, Parish, lat, lng = random_line.strip().split(', ')
-
-            return {
-                'sensor_group_name': sensor_group_name,
-                'Parish': Parish,
-                'lat': float(lat),
-                'lng': float(lng),
-            }
-    except FileNotFoundError:
-        print(f"The file '{file_path}' is not present.")
-    except Exception as e:
-        print(f"An unexpected error occurred while reading random data from the file: {e}")
-
-
-def write_data_to_file(data):
-    try:
-        with open(file_path, "a") as file1:
-            file1.write(','.join(data) + '\n')
-        print("Data written to the file.")
-    except FileNotFoundError:
-        print(f"The file '{file_path}' is not present. Creating the file...")
-        with open(file_path, "w") as file1:
-            file1.write(','.join(data) + '\n')
-        print(f"The file '{file_path}' has been created with initial data.")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-
-
-def delete_data_from_file(data):
-    try:
-        lines = []
-        with open(file_path, "r") as file1:
-            lines = file1.readlines()
-
-        with open(file_path, "w") as file1:
-            for line in lines:
-                if line.strip("\n") != ','.join(data):
-                    file1.write(line)
-
-        print(f"Data '{','.join(data)}' deleted from the file.")
-
-    except FileNotFoundError:
-        print(f"The file '{file_path}' is not present. Creating the file...")
-        with open(file_path, "w") as file1:
-            file1.write(','.join(data) + '\n')
-        print(f"The file '{file_path}' has been created with initial data.")
-
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
 
 
 def get_serial_data(sleep_sec=0):
     try:
         ser = serial.Serial('COM7', 9600, timeout=1)
         ser.flushInput()
-
         analog_dict = {
-            'A0': {'sensor_group': None, 'parish': None, 'lat': None, 'lng': None, 'soil_moisture_data': [],
-                   'timestamp': []},
-            'A1': {'sensor_group': None, 'parish': None, 'lat': None, 'lng': None, 'soil_moisture_data': [],
-                   'timestamp': []},
-            'A2': {'sensor_group': None, 'parish': None, 'lat': None, 'lng': None, 'soil_moisture_data': [],
-                   'timestamp': []},
+            'A0': {'soil_moisture_data': [], 'timestamp': []},
+            'A1': {'soil_moisture_data': [], 'timestamp': []},
+            'A2': {'soil_moisture_data': [], 'timestamp': []},
         }
 
         while True:
@@ -94,17 +34,8 @@ def get_serial_data(sleep_sec=0):
             if decoded_string and decoded_string != "":
                 output_list = decoded_string.split("\t")
                 for data_point in output_list:
-                    for prefix in ['A0:', 'A1:', 'A2:']:
+                    for prefix in ['A0:', 'A1:', 'A2:',]:
                         if prefix in data_point:
-                            # Use random data from the file
-                            random_data = get_random_data_from_file()
-
-                            # Update analog_dict with random data
-                            analog_dict[prefix[:-1]]['sensor_group'] = random_data['sensor_group_name']
-                            analog_dict[prefix[:-1]]['parish'] = random_data['Parish']
-                            analog_dict[prefix[:-1]]['lat'] = random_data['lat']
-                            analog_dict[prefix[:-1]]['lng'] = random_data['lng']
-
                             data_value = data_point.split(":")[1].strip()
                             timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                             data_dict = {
@@ -112,12 +43,11 @@ def get_serial_data(sleep_sec=0):
                                 'timestamp': timestamp
                             }
                             if data_value:
-                                analog_dict[prefix[:-1]]['soil_moisture_data'] = data_dict['soil_moisture_data']
-                                analog_dict[prefix[:-1]]['timestamp'] = data_dict['timestamp']
+                                analog_dict[prefix[:-1]] = data_dict
 
-            # process_data(analog_dict)
+            #process_data(analog_dict)
             print(analog_dict)
-            # logging.info(analog_dict)
+            logging.info(analog_dict)
 
             sleep(sleep_sec)
 
@@ -199,55 +129,5 @@ def main():
     get_serial_data()
 
 
-def listening_server():
-    host = '127.0.0.1'
-    port = 1234
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(5)
-
-    print(f"Server listening on {host}:{port}")
-
-    while True:
-        client_socket, addr = server_socket.accept()
-        print(f"Connection from {addr}")
-
-        # Handle client communication in a separate thread
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-        client_thread.start()
-
-
-def handle_client(client_socket):
-    try:
-        # Handle communication with the connected client
-        while True:
-            data = client_socket.recv(1024)
-            if not data:
-                break
-            else:
-                for key, csv_values in data.items():
-                    values_list = [value.strip() for value in csv_values.split(',')]
-
-                if values_list[0].lower() == 'write':
-                    write_data_to_file(values_list[1:])
-                elif values_list[0].lower() == 'delete':
-                    delete_data_from_file(values_list[1:])
-                else:
-                    print(f"Invalid instruction: {values_list[0]}")
-
-    except Exception as e:
-        logging.error(f"Error handling client: {e}")
-    finally:
-        client_socket.close()
-        print("Client disconnected")
-
-
 if __name__ == '__main__':
-    try:
-        threading.Thread(target=listening_server).start()
-    except Exception as e:
-        logging.error(e)
-
-    #get_serial_data()
-    # main()
+    main()
